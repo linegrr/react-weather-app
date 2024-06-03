add4887f1c8610d30e951580f9e80fed8bbebeda
linegrr <line.granseth@gmail.com>
2024-06-03 12:46:09 +0200

    added axios

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Weather />
+        <Weather defaultCity="Oslo" />
         
         <footer>This project was coded by  {" "}
         Line Granseth and is {" "} <a href="https://github.com/linegrr/react-weather-app" target='blank'> open-sourced on GitHub</a> </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,77 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 import "./Weather.css";
+import axios from "axios";
 
 
 export default function Weather() {
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-              />
-              </div>
-              <div className="col-3"> 
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary "
-              />
-            </div>
-          </div>
-        </form>
-        <h1> New York </h1>
-        <ul>
-          <li> Friday 12:00 </li>
-          <li> Mostly cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="mostly cloudy"
-            />{" "}
-            <span className="temperature">6 </span>
+
+const [weatherData, setWeatherData] = useState({ready: false});
+function handleReponse(response) {
+    setWeatherData({
+        ready: true,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: "Friday 12:00",
+        description: response.data.weather[0].description,
+        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        wind: response.data.main.wind.speed,
+        city: response.data.name });
+
+
+}
+if (weatherData.ready) {
+return (
+  <div className="Weather">
+    <form>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control"
+            autoFocus="on"
+          />
+        </div>
+        <div className="col-3">
+          <input type="submit" value="Search" className="btn btn-primary " />
+        </div>
+      </div>
+    </form>
+    <h1> {weatherData.city} </h1>
+    <ul>
+      <li> {date} </li>
+      <li className="text-capitalize"> {weatherData.description}</li>
+    </ul>
+    <div className="row mt-3">
+      <div className="col-6">
+        <div className="clearfix">
+          <img
+            src={weatherData.iconUrl}
+            alt={weatherData.description}
+            className="float-left"
+          />
+          <div className="float-left">
+            <span className="temperature">{Math.round(temperature)} </span>
             <span className="unit">℃</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li> Precipitation: 15%</li>
-              <li> Humidity: 72% </li>
-              <li> Wind: 13km/h</li>
-            </ul>
           </div>
         </div>
       </div>
-    );
+      <div className="col-6">
+        <ul>
+          
+          <li> Humidity: {weatherData.humidity} </li>
+          <li> Wind: {weatherData.wind}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+} else {
+const apiKey = "6f578b96aa9505bcce148ac22cb85794";
+    let city = "London";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleReponse);
+
+
+}
 }
