a4da24284acb8e8841ce6dd68cbc189aa5eaf3ec
linegrr <line.granseth@gmail.com>
2024-06-03 09:43:56 +0200

    added html weather

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import Weather from "./Weather";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className='container'>
+        <Weather />
+        
+        <footer>This project was coded by  {" "}
+        Line Granseth and is {" "} <a href="https://github.com/linegrr/react-weather-app" target='blank'> open-sourced on GitHub</a> </footer>
+      </div>
     
     </div>
   );
 }
 
-export default App;
+
---
+++ src/Weather.js
@@ -0,0 +1,43 @@
+import React from "react"; 
+import "./Weather.css";
+
+
+export default function Weather() {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+          </div>
+        </form>
+        <h1> New York </h1>
+        <ul>
+          <li> Friday 12:00 </li>
+          <li> Mostly cloudy</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="mostly cloudy"
+            />{" "}
+            6℃
+          </div>
+          <div className="col-6">
+            <ul>
+              <li> Precipitation: 15%</li>
+              <li> Humidity: 72% </li>
+              <li> Wind: 13km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+}
