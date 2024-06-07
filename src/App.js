40ecccdeca94d79acb2cf54b020a2d95fa899f48
linegrr <line.granseth@gmail.com>
2024-06-07 11:38:17 +0200

    minor adjustments

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
         <Weather defaultCity="Oslo" />
         
         <footer>This project was coded by  {" "}
-        Line Granseth and is {" "} <a href="https://github.com/linegrr/react-weather-app" target='blank'> open-sourced on GitHub</a> </footer>
+        Line Granseth and is {" "} <a href="https://github.com/linegrr/react-weather-app" target='blank'> open-sourced on GitHub</a> and hosted on <a href="https://peppy-panda-0d424c.netlify.app/">Netlify.</a> </footer>
       </div>
     
     </div>
--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="d-flex">
             <div>
-              <WeatherIcon code={props.data.icon} size={52} />
+              <WeatherIcon code={props.data.icon} size={50} />
             </div>
 
             <div>
@@ -28,7 +28,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
           </ul>
         </div>
       </div>
