207fa44d726a369c434804533d43e40724dd6ab2
linegrr <line.granseth@gmail.com>
2024-06-14 09:07:10 +0200

    Removed Fahrenheit from the weather app

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,38 +4,17 @@ import React,{ useState } from "react";
 export default function WeatherTemperature (props) {
     const [unit, setUnit] = useState("celcius");
 
-    function convertToFahrenheit(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
+    
 
-    function convertToCelcius(event) {
-        event.preventDefault();
-        setUnit("celcius");
-    }
 
 
     if (unit === "celcius") {
       return (
         <div className="WeatherTemperature">
           <span className="temperature">{Math.round(props.celcius)}</span>
-          <span className="unit">
-            ℃ |<a className="units" href="/"onClick={convertToFahrenheit}>℉</a>{" "}
-          </span>
+           <span className="unit">°C</span>
+          
         </div>
       );
-    } else {
-        let fahrenheit = (props.celcius * 9/5) +32
-        return (
-          <div className="WeatherTemperature">
-            <span className="temperature">{Math.round(fahrenheit)}</span>
-            <span className="unit">
-              ℉ |
-              <a href="/" onClick={convertToCelcius}>
-                ℃
-              </a>{" "}
-            </span>
-          </div>
-        );
-    }
+    } 
 }
